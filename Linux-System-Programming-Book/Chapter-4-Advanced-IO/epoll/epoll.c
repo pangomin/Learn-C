5b65e5682503c53c0589e8bb012237308dff498c
pangomin <pangominn@gmail.com>
2026-06-23 08:40:49 +0330

    epoll: implement read loop and fix epoll_ctl setup
    Include <errno.h> for error handling
    Set event.data.fd before calling epoll_ctl
    Use correct return types, ssize_t, for I/O operations
    Add proper error handling and exit failure
    Initialize buffer for read

--- Linux-System-Programming-Book/Chapter-4-Advanced-IO/epoll/epoll.c
+++ Linux-System-Programming-Book/Chapter-4-Advanced-IO/epoll/epoll.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/epoll.h>
+#include <errno.h>
 
 int main(void)
 {
@@ -14,18 +15,42 @@ int main(void)
 	}
 
 	int fd = STDIN_FILENO;
-	int ret;
+	ssize_t nr;
 	event.events = EPOLLIN;
-	ret = epoll_ctl(epfd, EPOLL_CTL_ADD, fd, &event);
-	if (ret)
+	event.data.fd = fd;
+	nr = epoll_ctl(epfd, EPOLL_CTL_ADD, fd, &event);
+	if (nr) {
 		perror("epoll_ctl");
+		return 1;
+	}
 	int nr_events;
+	ssize_t ret;
+	char buf[100];
+	char *buf_ptr = buf;
+	ssize_t len = sizeof(buf);
+	for (;;) {
 		nr_events = epoll_wait(epfd, &event, 1, 1000);
 		if (nr_events < 0) {
 			perror("epoll_wait");
 			return 1;
 		}
-		printf("%d", nr_events);
+		while (len != 0 && (ret = read(event.data.fd, buf_ptr, len != 0)))
+		{
+		if (ret == 0) {
+			break;
+		}
+		if (ret == -1) {
+			if (errno == EINTR) {
+				continue;
+		}
+			perror("read");
+			return 1;
+			}
+			len -= ret;
+			buf_ptr += ret;
+		}
+	}
+	close(epfd);
 
 	return 0;
 }
