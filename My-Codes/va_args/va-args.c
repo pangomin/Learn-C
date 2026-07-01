24ac254499630510ec2c08e1bd9f8f1bbbef06ae
pangomin <pangominn@gmail.com>
2026-07-01 07:50:14 +0330

    Add va-args.c

---
+++ My-Codes/va_args/va-args.c
@@ -0,0 +1,29 @@
+#include <stdarg.h>
+#include <stdio.h>
+
+//	va_start(ap, paramN); // va_arg and va_end will use ap
+
+int sum(int count, ...) {
+	int total = 0;
+	va_list ap;
+	va_start(ap, count);
+
+	for(int i = 0; i < count; i++) {
+		total+= va_arg(ap, int);
+	}
+	va_end(ap);
+
+	return total;
+
+}
+
+#define SQUARE(T, x) ((T)((x) * (x)))
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+
+int main(void) {
+	printf("%d\n", SQUARE(int, 2));
+	printf("%d\n", MAX(1, 1));
+
+	printf("%d\n", sum(2, 20, 30));
+	return 0;
+}
