e32653353e2cc5ffe0bf8bbdb733581d88ce32f7
pangomin <pangominn@gmail.com>
2026-07-16 22:09:01 +0330

    Add waitpid.c

---
+++ Linux-System-Programming-Book/Chapter-5-ProcessManagement/wait/waitpid.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	int status;
+	pid_t pid = fork();
+	pid_t waitpd;
+
+	if(pid == -1) {
+		perror("fork");
+		return 1;
+	}
+	if(pid == 0) {
+		printf("childpid=%d\n", getpid());
+		printf("parentpid=%d\n", getppid());
+	}
+	if(pid > 0) {
+		waitpd = waitpid(pid, &status, WNOHANG);
+		if(waitpd == -1) {
+			perror("waitpid");
+			return 1;
+		}
+		if(waitpd == 0) {
+			puts("Child is still running");
+		} else {
+			printf("waitpid=%d\n", waitpd);
+			if(WIFEXITED(status)) {
+				printf("Normal termination with exit status: %d\n",
+					WEXITSTATUS(status));
+			}
+		}
+	}
+	return 0;
+}
