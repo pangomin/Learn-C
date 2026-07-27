b9d904bbf291001d53a8179d880b5d99880ac61c
pangomin <pangominn@gmail.com>
2026-07-27 07:08:55 +0330

    Fix scheduling issue temporary

--- Linux-System-Programming-Book/Chapter-5-ProcessManagement/session/kill-children.c
+++ Linux-System-Programming-Book/Chapter-5-ProcessManagement/session/kill-children.c
@@ -1,6 +1,7 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <signal.h>
+#include <sched.h>
 
 int main(void) {
 	printf("Parent pid=%d pgid=%d\n", getpid(), getpgid(0));
@@ -10,6 +11,7 @@ int main(void) {
 	}
 	if(pid > 0) {
 		pid_t pid2 = fork();
+
 		if(pid2 > 0) {
 			if(setpgid(pid, pid) == -1) {
 				perror("setpgid");
@@ -22,19 +24,22 @@ int main(void) {
 		if(pid2 == -1) {
 			perror("fork"); return -1;
 		}
+
 		if(pid2 == 0) {
+			sleep(3);
 			while(1) {
 				printf("pid2 %d\npgid %d\n", getpid(), getpgid(0));
-				sleep(2);
 				kill(getpid(), SIGTERM);
 			}
 		}
 	}
 	if(pid == 0) {
+		sleep(3);
 		while(1) {
 			printf("pid %d\npgid %d\n", getpid(), getpgid(0));
-			sleep(2);
-			kill(pid, SIGTERM);
+			kill(getpid(), SIGTERM);
 		}
 	}
+	sleep(6);
+	return 0;
 }
